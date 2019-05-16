150a56f2b0b6d4c993d93fb922eef6883e25c52f
Gábor Czibik <gabor@czibik.hu>
2019-05-16 12:26:44 +0200

    Add project files.

---
+++ UpperTextBox/HtmlUpperTextBoxAdapter.cs
@@ -0,0 +1,17 @@
+﻿using Tricentis.Automation.Creation;
+using Tricentis.Automation.Engines.Adapters.Attributes;
+using Tricentis.Automation.Engines.Adapters.Html;
+using Tricentis.Automation.Engines.Technicals.Html;
+
+namespace UpperTextBox
+{
+    [SupportedTechnical(typeof(IHtmlInputElementTechnical))]
+    public class HtmlUpperTextBoxAdapter : HtmlTextBoxAdapter
+    {
+        protected HtmlUpperTextBoxAdapter(IHtmlInputElementTechnical htmlTechnical, Validator validator) : base(htmlTechnical, validator)
+        {
+        }
+
+        public override string Text { get => base.Text; set => base.Text = value?.ToUpper(); }
+    }
+}
